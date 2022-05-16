5345e4d0f6f28e76bd307bdc4ceab6584a957e34
Abhishek Verma <abhishekvermalmp260@gmail.com>
2022-05-16 17:01:15 +0530

    final commit

--- Hello.java
+++ Hello.java
@@ -1,6 +1,9 @@
+import java.util.*;
 public Hello{
  public static void main(String [] args ){
-  System.out.println("Hi This is Abhishek");
+  Scanner sc = new Scanner(System.in);
+  String name = sc.nextLine();
+  System.out.println("Hi This is "+name);
   System.out.println(" From Lucknow");
     }
  }
